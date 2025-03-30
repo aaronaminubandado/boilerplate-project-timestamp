dddbc3325c1b411f42cf42da42372e5119346cbd
Aaron Aminu Bandado <aaron.aminu1@gmail.com>
2025-03-30 12:32:54 +0000

    fix second case

--- index.js
+++ index.js
@@ -1,59 +1,49 @@
 // index.js
-// where your node app starts
 
 // init project
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  
+
+
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
-// your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/", function (req, res) {
-  const date = new Date();
+app.get("/api/:date?", function (req, res) {
+    let date;
 
+    if(!req.params.date){
+        date = new Date();
+    }else{
+        if (!isNaN(req.params.date)) {
+            date = new Date(parseInt(req.params.date))
+        } else {
+            date = new Date(req.params.date)
+        }
+    }
 
-  if (!isNaN(date.getTime())) { 
-      const unix = date.getTime();
-      const utc = date.toUTCString();
-      res.json({ unix, utc });
-  } else {
-      res.json({ error: 'Invalid Date' });
-  }
+    if(date.toString() === 'Invalid Date'){
+        res.json({ error: date.toString() });
+    }
+    else{
+        const unix = date.getTime();
+        const utc = date.toUTCString();
+        res.json({ unix, utc });
+    }
+
 });
-
-app.get("/api/:date", function (req, res) {
-  const date = new Date(req.params.date);
-
-  if(req.params.date.length === 0){
-      date = new Date();
-  }
-
-  if (!isNaN(date.getTime())) { 
-      const unix = date.getTime();
-      const utc = date.toUTCString();
-      res.json({ unix, utc });
-  } else {
-      res.json({ error: 'Invalid Date' });
-  }
-});
-
-
 
 
 // Listen on port set in environment variable or default to 3000
